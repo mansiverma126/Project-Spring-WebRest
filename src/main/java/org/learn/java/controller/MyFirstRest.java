dd14b2b81e240f30d7933ab4f5917e02d41bfad5
Mansi Verma <mansiverma126@gmail.com>
2022-07-06 19:22:46 -0400

    First Commit

---
+++ src/main/java/org/learn/java/controller/MyFirstRest.java
@@ -0,0 +1,16 @@
+package org.learn.java.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/mvc")
+public class MyFirstRest {
+
+	@RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public String hello() {
+		return "Our Readings to you from Spring MVC Application";
+	}
+}
